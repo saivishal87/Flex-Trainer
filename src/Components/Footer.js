906f612a19ae347e403477da71ec8826e65d34b4
Vishal <saivishalbellamkonda@gmail.com>
2023-07-31 19:43:40 +0530

    changed image

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Box, Stack, Typography } from '@mui/material';
-import Logo from '../assets/images/Logo.png';
+import Logo from '../assets/images/Flex Trainer logo.png';
 
 const Footer = () => (
   <Box mt="80px" bgcolor='#464D53'>
